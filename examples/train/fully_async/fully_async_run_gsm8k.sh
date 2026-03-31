4be6d6a32a89c46a5c8e7804b294a3b00b7082a3
Xiaohan Zhang <xiaohanzhang.cmu@gmail.com>
2026-03-31 06:55:40 +0000

    training starts

--- examples/train/fully_async/fully_async_run_gsm8k.sh
+++ examples/train/fully_async/fully_async_run_gsm8k.sh
@@ -16,8 +16,8 @@ set -x
 # You can override the default values with e.g.: `NUM_GPUS=1 bash examples/train/fully_async/fully_async_run_gsm8k.sh`.
 
 : "${DATA_DIR:="$HOME/data/gsm8k"}"
-: "${NUM_INFERENCE_GPUS:=2}"
-: "${NUM_POLICY_GPUS:=2}"
+: "${NUM_INFERENCE_GPUS:=4}"
+: "${NUM_POLICY_GPUS:=4}"
 : "${LOGGER:=wandb}" # change to "console" to print to stdout / or use wandb
 
 : "${INFERENCE_BACKEND:=vllm}"
